3aee12860a5b336f434475217f3ec71bd9f14b98
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-07-14 20:44:39 +1000

    Update decoration_cv.m

--- decoration_cv.m
+++ decoration_cv.m
@@ -2,7 +2,7 @@
 
 % ----------------ChangeLog---------------
 % v1. 6/7/19 Initial creation with comments and brief summary. Image capture
-% v2. 9/7/19 Modified structure and made basic function
+% v2. 
 
 % ----------------ChangeLog---------------
 
@@ -19,41 +19,34 @@
 
 %% Decoration Computer Vision Pipeline
 
-% i. RobotCell Image
+% 1. Customer Image @ Robot CEll
 
-blockImage = webcam('USB Video Device'); 
-capture_image(blockImage,'Robot Image');
-clear('blockImage');
+%(LATER ON USE ROBOT CELL CAMERA)
+% Testing with customer's sample image: 
+customerImage = imread('sample1.jpg');
+imshow(customerImage)
 
-% 1. Obtain customer's image
-
-%customerImage = imread('sample1.jpg');
-%imshow(customerImage)
-
-% 2. Identify desired 'blocks' (Quirkle colored shapes) on customer's image
+% 2. Identify desired 'blocks' (color + shape + location/rotation)
 
 % 3. Results of classification (which color and which shape)
 
-% 4. x
+% 4. Conveyor block detection
 
 
 % FUNCTIONS
 
 function capture_image (vid,name)
- 	robotCellImage = snapshot(vid);
-    imshow(robotCellImage);
-    imwrite(robotCellImage, [name, datestr(datetime('now'),'_mm_dd_HH_MM_SS'), '.jpg']);
-    disp([name 'Complete']);
+  snapshot = getsnapshot(vid);
+  imwrite(snapshot, [name, datestr(datetime('now'),'_mm_dd_HH_MM_SS'), '.jpg']);
+  disp([name 'captured']);
 end
 
-
-%Tried running this function on Robot cell. Had issue with the format
-%of the image when trying to take image. (9/7/2019)
-function RobotCellCamera (varargin)
+function MTRN4230_Image_Capture (varargin)
     close all;
     warning('off', 'images:initSize:adjustingMag');
 
-    %Table Camera
+%Table Camera (Robot Cell)
+% {
     if nargin == 0 || nargin == 1
         fig1 =figure(1);
         axe1 = axes ();
@@ -61,15 +54,30 @@ function RobotCellCamera (varargin)
         vid1 = videoinput('winvideo', 1, 'MJPG_1600x1200');
         video_resolution1 = vid1.VideoResolution;
         nbands1 = vid1.NumberOfBands;
-        img1 = imshow(zeros([video_resolution1(2), video_resolution1(1), nbands1]), 'Parent', axe1); 
-        prev1 = preview(img1);
+        img1 = imshow(zeros([video_resolution1(2), video_resolution1(1), nbands1]), 'Parent', axe1);
+        prev1 = preview(vid1,img1);
         src1 = getselectedsource(vid1);
-        src1.ExposureMode = 'manual';
-        src1.Exposure = -4;
-%         src1.Contrast = 57;%57,32
-%         src1.Saturation = 78;
         cam1_capture_func = @(~,~)capture_image(vid1,'table_img');
         prev1.ButtonDownFcn = cam1_capture_func;
         fig1.KeyPressFcn = cam1_capture_func;
-    end   
+    end
+%}
+% Conveyor Camera
+% {
+    if nargin == 0 || nargin == 2
+        fig2 =figure(2);
+        axe2 = axes ();
+        axe2.Parent = fig2;
+        vid2 = videoinput('winvideo', 2, 'MJPG_1600x1200');
+        video_resolution2 = vid2.VideoResolution;
+        nbands2 = vid2.NumberOfBands;
+        img2 = imshow(zeros([video_resolution2(2), video_resolution2(1), nbands2]), 'Parent', axe2);
+        prev2 = preview(vid2,img2);
+        src2 = getselectedsource(vid2);
+        cam2_capture_func = @(~,~)capture_image(vid2,'conveyor_img');
+        fig2.KeyPressFcn = cam2_capture_func;
+        prev2.ButtonDownFcn = cam2_capture_func;
+    end
+%}
 end
+
