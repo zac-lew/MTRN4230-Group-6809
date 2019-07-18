c66864d5aa8c7a0a8d463c4c6019553cbde8bd10
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-07-18 21:49:56 +1000

    Big changes to structure and pipeline

--- decoration_cv.m
+++ decoration_cv.m
@@ -2,62 +2,112 @@
 
 % ----------------ChangeLog---------------
 % v1. 6/7/19 Initial creation with comments and brief summary. Image capture
-% v2. 
+% v2. 15/7/19 Improved image capture at robot cell
+% v3. 18/7/19 Added in structure for YOLOv2 network
 
 % ----------------ChangeLog---------------
 
 % Computer Vision Engineer (Decoration)
 % 1. Detect Quirkle blocks as they are supplied using the conveyor (ML)
-% 2. Detect the customer's desired decorating pattern from conveyor camera (CV)
+% 2. Detect the customer's desired decorating pattern from robot cell camera (ML/CV)
 
 % Customer's IMAGE::
 % Location = x,y for blocks? (target locations)
 % Type of Block = Shape + Colour
 
 % Shapes == (criss cross, clover, starburst, square, diamond, circle}
-% Color == (red, blue, green, yellow, purple, orange)
+% Color == (red, blue, green, yellow)
 
-%% Decoration Computer Vision Pipeline
+%~~Decoration Computer Vision Pipeline~~
 
-% 1. Customer Image @ Robot CEll
+%% 1. Obtain Customer Image @ Robot CEll
 
-%(LATER ON USE ROBOT CELL CAMERA)
-% Testing with customer's sample image: 
-customerImage = imread('sample1.jpg');
+% Testing with customer's sample image: (sample2.jpg)
+customerImage = imread('sample1.png');
 imshow(customerImage)
 
-% 2. Identify desired 'blocks' (color + shape + location/rotation)
+%(LATER ON USE ROBOT CELL CAMERA)
+%MTRN4230_Image_Capture([]) %for robot cell
 
-% 3. Results of classification (which color and which shape)
+%% 2. Quirkle Blocks from Sample Image (Classification + Localisation)
+% YOLOv2 Detection Network for Quirkle Blocks
 
-% 4. Conveyor block detection
+doTraining = true;
 
+% ------------------------SET UP TRAINING and TEST SETS----------------
 
-% FUNCTIONS
+% 1. Load Training Data
+
+% 2. Show Sample Training Data Image
+
+% 3. Create YOLOv2 Detection Network
+
+% Create test and training data sets
+% Set random seed to ensure example training reproducibility.
+rng(0);
+
+% Randomly split data into a training and test set.
+shuffledIndices = randperm(height(vehicleDataset));
+idx = floor(0.6 * length(shuffledIndices) );
+trainingData = vehicleDataset(shuffledIndices(1:idx),:);
+testData = vehicleDataset(shuffledIndices(idx+1:end),:);
+
+% ------------------------CREATE DETECTION NETWORK-------------------
+
+% YOLOv2 = feature network (pre-trained CNN) & detection network (smaller
+% CNN specific to YOLO)
+
+% The image input size should be at least as big as the images in the training image set.
+% Define the image input size. 224 x 224 pixels RGB images
+imageSize = [224 224 3];
+
+% Define the number of object classes to detect.
+numClasses = width(vehicleDataset)-1;
+
+% ------------------------TEST MODEL ON TEST IMAGES--------------------
+
+% Read a test image.
+I = imread(testData.imageFilename{end});
+
+% Run the detector.
+[bboxes,scores] = detect(detector,I);
+
+% Annotate detections in the image.
+I = insertObjectAnnotation(I,'rectangle',bboxes,scores);
+imshow(I)
+
+%% 3. Orientation of Blocks
+
+% Image processing to determine orientation of blocks
+
+%% 4. Detect on Conveyor Belt (Object Detection)
+
+% Change to conveyor camera
+%MTRN4230_Image_Capture([],[]) %for conveyor camera
+
+%% FUNCTIONS
 
 function capture_image (vid,name)
   snapshot = getsnapshot(vid);
   imwrite(snapshot, [name, datestr(datetime('now'),'_mm_dd_HH_MM_SS'), '.jpg']);
-  disp([name 'captured']);
+  disp([name ' Captured']);
 end
 
 function MTRN4230_Image_Capture (varargin)
     close all;
-    warning('off', 'images:initSize:adjustingMag');
-
 %Table Camera (Robot Cell)
 % {
     if nargin == 0 || nargin == 1
         fig1 =figure(1);
         axe1 = axes ();
         axe1.Parent = fig1;
-        vid1 = videoinput('winvideo', 1, 'MJPG_1600x1200');
+        vid1 = videoinput('winvideo', 1, 'MJPG_1280x720');
         video_resolution1 = vid1.VideoResolution;
         nbands1 = vid1.NumberOfBands;
         img1 = imshow(zeros([video_resolution1(2), video_resolution1(1), nbands1]), 'Parent', axe1);
         prev1 = preview(vid1,img1);
         src1 = getselectedsource(vid1);
-        cam1_capture_func = @(~,~)capture_image(vid1,'table_img');
+        cam1_capture_func = @(~,~)capture_image(vid1,'QBlock_RC');
         prev1.ButtonDownFcn = cam1_capture_func;
         fig1.KeyPressFcn = cam1_capture_func;
     end
@@ -74,7 +124,7 @@ function MTRN4230_Image_Capture (varargin)
         img2 = imshow(zeros([video_resolution2(2), video_resolution2(1), nbands2]), 'Parent', axe2);
         prev2 = preview(vid2,img2);
         src2 = getselectedsource(vid2);
-        cam2_capture_func = @(~,~)capture_image(vid2,'conveyor_img');
+        cam2_capture_func = @(~,~)capture_image(vid2,'QBlock_CON');
         fig2.KeyPressFcn = cam2_capture_func;
         prev2.ButtonDownFcn = cam2_capture_func;
     end
