a21f6ac96633f2f7783198537182b431dd99c57e
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-07-20 16:10:35 +1000

    Add in centroid detection/points experiments

--- decoration_cv.m
+++ decoration_cv.m
@@ -4,7 +4,6 @@
 % v1. 6/7/19 Initial creation with comments and brief summary. Image capture
 % v2. 15/7/19 Improved image capture at robot cell
 % v3. 18/7/19 Added in structure for YOLOv2 network
-
 % ----------------ChangeLog---------------
 
 % Computer Vision Engineer (Decoration)
@@ -36,21 +35,20 @@ doTraining = true;
 
 % ------------------------SET UP TRAINING and TEST SETS----------------
 
-% 1. Load Training Data
+% Load Training Data (using ImageLabeller - ground truth)
+%data = load('quirkleGroundTruth.mat');
+%QuirkleDataset = data.quirkleDataset;
 
-% 2. Show Sample Training Data Image
+% Show Sample Training Data Image
 
-% 3. Create YOLOv2 Detection Network
-
-% Create test and training data sets
 % Set random seed to ensure example training reproducibility.
 rng(0);
 
 % Randomly split data into a training and test set.
-shuffledIndices = randperm(height(vehicleDataset));
+shuffledIndices = randperm(height(QuirkleDataset));
 idx = floor(0.6 * length(shuffledIndices) );
-trainingData = vehicleDataset(shuffledIndices(1:idx),:);
-testData = vehicleDataset(shuffledIndices(idx+1:end),:);
+trainingData = QuirkleDataset(shuffledIndices(1:idx),:); %60
+testData = QuirkleDataset(shuffledIndices(idx+1:end),:); %40
 
 % ------------------------CREATE DETECTION NETWORK-------------------
 
@@ -58,11 +56,13 @@ testData = vehicleDataset(shuffledIndices(idx+1:end),:);
 % CNN specific to YOLO)
 
 % The image input size should be at least as big as the images in the training image set.
-% Define the image input size. 224 x 224 pixels RGB images
-imageSize = [224 224 3];
+% Define the image input size.
+imageSize = [80 80 3]; %might need to resize but are RBG
 
-% Define the number of object classes to detect.
-numClasses = width(vehicleDataset)-1;
+% Define the number of object classes to detect. (6 shapes == 6 classes)
+numClasses = width(QuirkleDataset)-1; %5
+
+% Create rest of YOLOv2 Detection Network....
 
 % ------------------------TEST MODEL ON TEST IMAGES--------------------
 
@@ -76,11 +76,79 @@ I = imread(testData.imageFilename{end});
 I = insertObjectAnnotation(I,'rectangle',bboxes,scores);
 imshow(I)
 
-%% 3. Orientation of Blocks
+% Test Detector with test set
+
+% Create a table to hold the bounding boxes, scores, and labels output by
+% the detector. 
+numImages = height(testData);
+results = table('Size',[numImages 3],...
+    'VariableTypes',{'cell','cell','cell'},...
+    'VariableNames',{'Boxes','Scores','Labels'});
+
+% Run detector on each image in the test set and collect results.
+for i = 1:numImages
+    
+    % Read the image.
+    I = imread(testData.imageFilename{i});
+    
+    % Run the detector.
+    [bboxes,scores,labels] = detect(detector,I);
+   
+    % Collect the results.
+    results.Boxes{i} = bboxes;
+    results.Scores{i} = scores;
+    results.Labels{i} = labels;
+end
+
+% Extract expected bounding box locations from test data.
+expectedResults = testData(:, 2:end);
+
+% Evaluate the object detector using average precision metric.
+[ap, recall, precision] = evaluateDetectionPrecision(results, expectedResults);
+
+%% 3. Color Filtering + Localisation
+
+% Testing with customer's sample image: (sample2.jpg)
+customerImage = imread('sample1.png');
+figure
+imshow(customerImage)
+
+% %[a,b] = imcrop(customerImage);
+% rectROI = [311.51,173.51,621.98,462.98]; 
+% ROI_image = imcrop(customerImage,rectROI);
+% figure
+% imshow(ROI_image)
+
+% i. ---------------Find RED Object-------------
+
+%HSV
+hsv_path = rgb2hsv(customerImage);
+hsv_path = imgaussfilt(hsv_path,0.5);
+
+% Threshold for HSV - Green Square (END)
+low_red = [0.825,0.475,0.2];
+hi_red = [1.00,1.00,1.00];
+
+% Create mask to find pixels with desired HSV ranges (binary mask)
+mask_red = (hsv_path(:,:,1) >= low_red(1)) & (hsv_path(:,:,1) <= hi_red(1)) & ...
+    (hsv_path(:,:,2) >= low_red(2) ) & (hsv_path(:,:,2) <= hi_red(2)) & ...
+    (hsv_path(:,:,3) >= low_red(3) ) & (hsv_path(:,:,3) <= hi_red(3));
+
+stats = regionprops(mask_red,'basic');
+centroids = cat(1,stats.Centroid);
+areas = cat(1,stats.Area);
+[m_red,i_red] = max(areas(:,1)); %largest contour
+start_circle_x = centroids(i_red,1);
+start_circle_y = centroids(i_red,2);
+hold on
+plot(start_circle_x,start_circle_y,'bo','LineWidth',2)
+hold off
+
+%% 4. Orientation of Blocks
 
 % Image processing to determine orientation of blocks
 
-%% 4. Detect on Conveyor Belt (Object Detection)
+%% 5. Detect on Conveyor Belt (Object Detection)
 
 % Change to conveyor camera
 %MTRN4230_Image_Capture([],[]) %for conveyor camera
