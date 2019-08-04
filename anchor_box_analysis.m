8506bf3bba5fe3d3efd597bfe83d763269b84137
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-08-04 18:37:10 +1000

    Create anchor_box_analysis.m

---
+++ anchor_box_analysis.m
@@ -0,0 +1,59 @@
+% Analysing anchor box metrics for YOLOv2
+
+load('trainingData.mat');
+
+% Combine all the ground truth boxes into one array.
+allBoxes = vertcat(trainingData.Circle{:}, trainingData.Clover{:},...
+                trainingData.CrissCross{:}, trainingData.Diamond{:},...
+                trainingData.Square{:}, trainingData.Starburst{:});
+            
+% Plot the box area versus box aspect ratio.
+aspectRatio = allBoxes(:,3) ./ allBoxes(:,4);
+area = prod(allBoxes(:,3:4),2);
+
+figure
+scatter(area,aspectRatio)
+xlabel("Box Area")
+ylabel("Aspect Ratio (width/height)");
+title("Box area vs. Aspect ratio")
+
+% Select the number of anchor boxes.
+numAnchors = 4;
+
+% Cluster using K-Medoids.
+[clusterAssignments, anchorBoxes, sumd] = kmedoids(allBoxes(:,3:4),numAnchors,'Distance',@iouDistanceMetric);
+
+% Display estimated anchor boxes. The box format is the [width height].
+anchorBoxes
+
+% Display clustering results.
+figure
+gscatter(area,aspectRatio,clusterAssignments);
+title("K-Mediods with "+numAnchors+" clusters")
+xlabel("Box Area")
+ylabel("Aspect Ratio (width/height)");
+grid
+
+% Count number of boxes per cluster. Exclude the cluster center while
+% counting.
+counts = accumarray(clusterAssignments, ones(length(clusterAssignments),1),[],@(x)sum(x)-1);
+
+% Compute mean IoU.
+meanIoU = mean(1 - sumd./(counts))
+
+maxNumAnchors = 15;
+for k = 1:maxNumAnchors
+    
+    % Estimate anchors using clustering.
+    [clusterAssignments, anchorBoxes, sumd] = kmedoids(allBoxes(:,3:4),k,'Distance',@iouDistanceMetric);
+    
+    % Compute mean IoU.
+    counts = accumarray(clusterAssignments, ones(length(clusterAssignments),1),[],@(x)sum(x)-1);
+    meanIoU(k) = mean(1 - sumd./(counts));
+end
+
+figure
+plot(1:maxNumAnchors, meanIoU,'-o')
+ylabel("Mean IoU")
+xlabel("Number of Anchors")
+title("Number of Anchors vs. Mean IoU")
