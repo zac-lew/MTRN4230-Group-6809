3fb0dfeec05ca42dd5108c6ed06611023faadf75
Benjamin North <benjamin.north24@gmail.com>
2019-08-18 11:46:58 +1000

    Plot skeleton, triple points and end points

--- text_detection.m
+++ text_detection.m
@@ -1,6 +1,6 @@
 function text_detection()
     close all; 
-    basename = 'cake-design-photos/'; 
+    basename = '../cake-design-photos/'; 
     files = dir(strcat(basename, 'table*'));
     
     for f = files'
@@ -13,16 +13,17 @@ function text_detection()
 end
 
 function run_text_detection(img, filename)
-    %img = iread('w8-photos/table (31).jpg');
+    % img = iread('../w8-photos/table (5).jpg');
     grey = im2double(rgb2gray(img));
     grey = im2double(grey); % normalises greyscale to [0,1] range
     grid_th = getGridRoi(grey);
     blobs = getBlobs(grid_th);
+    triple_pts = getTriplePoints(grey, grid_th, blobs);
     
     % plot the blobs
-    figure; imshow(grid_th);
-    title(sprintf('%s, num blobs = %d', filename, length(blobs)));
-    blobs.plot_box;    
+    %figure; imshow(grid_th);
+    %title(sprintf('%s, num blobs = %d', filename, length(blobs)));
+    %blobs.plot_box;    
 end
 
 function grid_th = getGridRoi(grey)
@@ -60,4 +61,41 @@ function blobs = getBlobs(grid_th)
     % note: letters will be *at least* 50mm high. So do not rely on upper
     % area bound, or a specfic major axis height (though could have lower
     % bound). 
+end
+
+function triple_points = getTriplePoints(im, grid_th, blobs)
+    n = size(blobs, 2);
+    figure; idisp(grid_th);
+    hold on;
+    
+    for i = 1:n
+        % use a larger bbox to ensure the blob isn't touching the edges
+        roi = blobs(i).bbox + [-1 1; -1 1];
+        blob_im = iroi(grid_th, roi);
+        skeleton = ithin(1 - blob_im); % use inverse image
+        triple_points_log = itriplepoint(skeleton);
+        end_pts_log = iendpoint(skeleton);
+        
+        %[tp_y, tp_x] = find(triple_points_log); 
+        
+        %triple_points = [tp_x'; tp_y'];
+        % convert to picture coord frame, allowing for larger bbox
+        %triple_points = triple_points + [blobs(i).umin; blobs(i).vmin] ...
+        %    + [-1; -1];
+        
+        tp_offset = [blobs(i).umin; blobs(i).vmin] + [-2; -2];
+        triple_points = convertLogToPoints(triple_points_log, tp_offset);
+        loc_tps = convertLogToPoints(triple_points_log, zeros(2,1));
+        skel_points = convertLogToPoints(skeleton, tp_offset);
+        end_pts = convertLogToPoints(end_pts_log, tp_offset);
+        plot_point(skel_points, 'r.');
+        plot_point(triple_points, 'w+');
+        plot_point(end_pts, 'go');
+    end
+end
+
+function pts = convertLogToPoints(logical, offset) 
+    [y, x] = find(logical);
+    pts = [x'; y'];
+    pts = pts + offset;
 end
