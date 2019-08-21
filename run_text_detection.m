02f1a70113f9143a463820ab8261b673c7194445
alee969 <47757644+alee969@users.noreply.github.com>
2019-08-21 15:45:50 +1000

    error fixed

--- run_text_detection.m
+++ run_text_detection.m
@@ -15,7 +15,7 @@ function run_text_detection()
     imshow(img)
     [blob_paths, blob_im, n_blobs] = text_detection(img);
     
-    for i = 1:n_blobs - 4
+    for i = 1:n_blobs
         n_strokes = size(blob_paths{i}, 2);
         disp(i);
         % iterate through all the strokes for this blob
