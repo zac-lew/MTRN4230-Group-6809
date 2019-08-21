d9bf731bbb2d1c7e3f66cf7fef38c4c7a7189e09
alee969 <47757644+alee969@users.noreply.github.com>
2019-08-21 21:42:06 +1000

    loop fixed

--- run_text_detection.m
+++ run_text_detection.m
@@ -19,7 +19,7 @@ function run_text_detection()
     imshow(img)
     [blob_paths, blob_im, n_blobs] = text_detection(img);
     
-    for i = 1:n_blobs - 4
+    for i = 1:n_blobs
         n_strokes = size(blob_paths{i}, 2);
         disp(i);
         % iterate through all the strokes for this blob
@@ -37,6 +37,5 @@ function run_text_detection()
     while done ~= 1
         done = ALEndCommunications();
     end 
-    
 end
 
