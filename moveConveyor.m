59464765c88e73f299118e7ebe984e2d885c551d
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-08-24 23:34:11 +1000

    Create moveConveyor.m

---
+++ moveConveyor.m
@@ -0,0 +1,20 @@
+% Move Conveyor Forwards
+function moveConveyor(direction,enable)
+    
+    global socket_1;    
+
+    if (enable)
+        if (direction) %direction = true (forward towards robot)
+            %fwrite command for direction
+            fwrite(socket_1,'CON');
+            pause(0.50);
+            fwrite(socket_1,'COF');
+        else %direction = false (backward away from robot)
+            %fwrite command for direction
+            fwrite(socket_1,'CON');
+            pause(0.50);
+            fwrite(socket_1,'COF');
+        end
+    end
+end
+
