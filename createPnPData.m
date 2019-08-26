c2469ddfe0b913680522d6680677f13b50ecbdca
Jono453 <32658975+Jono453@users.noreply.github.com>
2019-08-26 23:52:38 +1000

    add in reverse conveyor logic

--- createPnPData.m
+++ createPnPData.m
@@ -1,6 +1,8 @@
-function guiString = createPnPData(tempJ,shape_color,X,Y,block_angleC)
+function [guiString,outOfReach,dir] = createPnPData(tempJ,shape_color,X,Y,block_angleC)
     
     global camParam_Conv R_Conv t_Conv;
+    outOfReach = false; %default
+    dir = 1; %default is forwards
     
     % i) PICK COORDINATES
     dataVector(1,1:2) = pointsToWorld(camParam_Conv,...
@@ -16,20 +18,26 @@ function guiString = createPnPData(tempJ,shape_color,X,Y,block_angleC)
     if (~canReach)
         disp('Not Reachable');
         % move conveyor back/forwards
-        if (dataVector(2) < -150) %TODO: Improve this if possible
-            moveConveyor(true,true); %direction = true (forward towards robot)
-        elseif (dataVector(2) > -50)  
-            moveConveyor(false,true); %direction = false (away from robot)
+        if (dataVector(1) < -100) %TODO: Improve this if possible
+            outOfReach = true; %direction = true (forward towards robot)
+            dir = 1;         
+            disp('move forward')
+        elseif (dataVector(1) > -60)  
+            outOfReach = true; %direction = false (away from robot)
+            dir = 0;
+            disp('move backward')
         end
-    end
+        guiString = '';
+    else
+        % if can reach
+         % ii) PLACE COORDINATES                            
+        dataVector(1,3) = shape_color(3,tempJ);
+        dataVector(1,4) = shape_color(4,tempJ);
 
-    % ii) PLACE COORDINATES                            
-    dataVector(1,3) = shape_color(3,tempJ);
-    dataVector(1,4) = shape_color(4,tempJ);
+        % iii) ANGLE                               
+        dataVector(1,5) = block_angleC - shape_color(5,tempJ);
 
-    % iii) ANGLE                               
-    dataVector(1,5) = block_angleC - shape_color(5,tempJ);
-
-    guiString = sendPnP(dataVector); %array-string HERE                  
-    fprintf('Sent %s to GUI!\n',guiString);
+        guiString = sendPnP(dataVector); %array-string HERE                  
+        fprintf('Sent %s to GUI!\n',guiString);
+    end   
 end
